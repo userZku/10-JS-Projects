5dc3edc39f2150566918ccc9127f7335814b4dc6
userZku <totoche22300@gmail.com>
2022-03-18 02:06:58 +0100

    020618032022

--- love-love/script.js
+++ love-love/script.js
@@ -1,37 +1,24 @@
-let HEART_LIST = ["💜", "🔥", "🥰", "🤣", "😜", "🥵", "😎", "🤗"];
+const EMOJI_LIST = ["💜", "💕", "💖", "💞", "❤️"];
 const txt = "Clément c'est vraiment le plus beau <3";
 
-let index = 0;
-
-function writeText() {
-    document.body.innerText = txt.slice(0, index);
-    index++;
-    if (index > txt.length) {
-        index = 0;
-    }
-}
-
 function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createHeart() {
-    const heart = document.createElement("div");
-    heart.classList.add("heart");
+function createEmoji() {
+  const emoji = document.createElement("div");
+  emoji.classList.add("emoji");
 
-    heart.style.left = Math.random() * 100 + "vw";
-    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+  emoji.style.left = Math.random() * 100 + "vw";
+  emoji.style.animationDuration = Math.random() * 2 + 3 + "s";
 
-    heart.innerText = HEART_LIST[randomInt(0, HEART_LIST.length - 1)];
+  emoji.innerText = EMOJI_LIST[randomInt(0, EMOJI_LIST.length - 1)];
 
-    document.body.appendChild(heart);
+  document.body.appendChild(emoji);
 
-    setTimeout(() => {
-        heart.remove();
-    }, 5000);
+  setTimeout(() => {
+    emoji.remove();
+  }, 5000);
 }
 
-setInterval(() => {
-    writeText();
-    createHeart();
-}, 100);
+setInterval(createEmoji, 300);
